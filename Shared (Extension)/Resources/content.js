8b7719054b84562c479954f2438a275340a52926
Daniel <daniel.razaviazarkhiavi@mail.utoronto.ca>
2023-03-14 17:39:23 -0400

    Made a MVP that showcases the ability and vision of the extension.

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -2,6 +2,25 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
     console.log("Received response: ", response);
 });
 
+//Content.js
+let value = 'false';
+let pageElements = ["secondary-inner", "comments"];
+
+const toggleDistraction = (value) => {
+    var element;
+    pageElements.forEach(elementId => {
+        element = document.getElementById(elementId);
+        if (value) {
+            element.style.display = "none";
+        } else {
+            element.style.display = "block";
+        }
+    });
+}
+
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+    value = request.value;
+    toggleDistraction(value);
+    return Promise.resolve({ isSuccessful: true });
+    
 });
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,42 @@
-console.log("Hello World!", browser);
+//popup.js
+const stateSwitchOn =
+{
+    name: "On",
+    value: true,
+    color: '#F5F5F5'
+}
+
+const stateSwitchOff =
+{
+    name: "Off",
+    value: false,
+    color: '#9E9E9E'
+}
+
+const sendStateSwitch = async (value) => {
+    const [tab] = await browser.tabs.query({currentWindow: true, active: true})
+    browser.tabs.sendMessage(tab.id, { value })
+}
+
+
+if (!stateSwitch){
+    var stateSwitch = stateSwitchOff;
+}
+
+const button = document.createElement('button');
+button.style.background = stateSwitch.color;
+button.innerText = stateSwitch.name;
+
+document.querySelector('#switch-container').appendChild(button);
+
+button.addEventListener('click', e => {
+    if (button.innerText == stateSwitchOff.name){
+        stateSwitch = stateSwitchOn;
+    } else {
+        stateSwitch = stateSwitchOff;
+    }
+    button.style.background = stateSwitch.color
+    button.innerText = stateSwitch.name
+    sendStateSwitch(stateSwitch.value)
+})
+
